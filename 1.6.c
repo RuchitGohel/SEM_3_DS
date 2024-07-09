dfead60945ee9dca73e077ba5c9f9668da02e8cc
RuchitGohel <141817181+RuchitGohel@users.noreply.github.com>
2024-07-09 10:36:40 +0530

    Add files via upload

--- 1.6.c
+++ 1.6.c
@@ -5,23 +5,20 @@
 
 void main()
 {
-    int a[10], pos, x, i;
+    int a[10], pos, i;
 
     for(i=0;i<10;i++)
     {
-            printf("\nEnter an element and its index you want to insert it at:");
-            scanf("%d", &x);
-            printf("\nEnter the position you want to insert it at:");
+            printf("\nEnter position:");
             scanf("%d", &pos);
-
-            i=pos;
-            a[i]=x;
+            printf("\nEnter value:");
+            scanf("%d", &a[pos-1]);
     }
 
     printf("\nArray :\n");
     for(i=0;i<10;i++)
     {
-        printf("%d ", a[i]);
+        printf("a[%d]=%d\n ",i, a[i]);
     }
 
 
