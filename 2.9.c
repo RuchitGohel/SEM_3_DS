9d7260716f9d6f1b014a3f2d298442aedfee51d4
RuchitGohel <141817181+RuchitGohel@users.noreply.github.com>
2024-08-09 10:33:27 +0530

    Add files via upload

---
+++ 2.9.c
@@ -0,0 +1,95 @@
+/*
+2.9 - Write a program which performs the following operations
+      using simple queue : insert() -> delete() -> display()
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+void add();
+void myRemove();
+void display();
+int queue[50];
+int max=20;
+int front=-1;
+int rear=-1;
+
+
+void main()
+{
+    int ch;
+    while(1)
+    {
+        printf("\n");
+        printf("1. add\n");
+        printf("2. remove\n");
+        printf("3. display\n");
+        printf("4. exit\n");
+
+
+        printf("Enter your choice:\n");
+        scanf("%d", &ch);
+
+        switch(ch)
+        {
+            case 1: add();
+            break;
+            case 2: myRemove();
+            break;
+            case 3: display();
+            break;
+            case 4: exit(1);
+            break;
+            default: printf("\nEnter proper choice.");
+        }
+        }
+}
+
+void add()
+{
+        int val;
+        if(rear==max-1)
+        {
+            printf("\nQueue is overflow.");
+        }
+        else if(rear==-1 && front==-1)
+        {
+            rear=0;
+            front=0;
+            printf("\nEnter value:");
+            scanf("%d", &val);
+            queue[rear]= val;
+        }
+        else
+            {
+
+                rear=rear+1;
+                printf("\nEnter value:");
+                scanf("%d", &val);
+                queue[rear]=val;
+            }
+}
+
+void myRemove()
+{
+    int val;
+    if(front>rear || front==-1)
+    {
+            printf("\nQueue is empty.");
+    }
+    else
+        {
+            val=queue[front];
+            printf("\nValue deleted.");
+            front++;
+        }
+}
+void display()
+{
+    int i;
+    for(i=front; i<=rear; i++)
+    {
+        printf("\n%d\n", queue[i]);
+    }
+}
